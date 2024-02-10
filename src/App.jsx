ffd9f845f1b8f58f8bcc68ddaa5788d36686f405
zahidmiran <zahidmiron599@gmail.com>
2024-02-10 16:21:58 +0600

    testing component using getByRole queries from react-testing-library

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import PrimaryButton from "./components/PrimaryButton";
+import Users from "./components/Users/User";
 
 const App = () => {
   return (
     <div>
       <h1>Hello World</h1>
-      <PrimaryButton />
-      <PrimaryButton actionType="Delete" />
+      {/* <PrimaryButton />
+      <PrimaryButton actionType="Delete" /> */}
+      <Users />
     </div>
   );
 };
---
+++ src/components/Users/user.test.jsx
@@ -0,0 +1,17 @@
+import { render, screen } from "@testing-library/react";
+import { expect, it } from "vitest";
+import Users from "./User";
+
+it("should render the component", () => {
+  render(<Users />);
+  const element = screen.getByRole("textbox", {
+    name: "Name",
+  });
+
+  const headingElement = screen.getByRole("heading", {
+    level: 2,
+  });
+
+  expect(element).toBeInTheDocument();
+  expect(headingElement).toBeInTheDocument();
+});
